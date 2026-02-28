74903c43165a403b6489fcaeb85f863a11107cd0
André Carlos <andrecarlos.miranda@gmail.com>
2026-02-27 21:02:04 -0300

    aaa

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'notas-v2';
+const CACHE_NAME = 'notas-v3';
 const ASSETS = [
   './',
   './index.html',
@@ -7,14 +7,32 @@ const ASSETS = [
   'https://cdn.tailwindcss.com'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+// Instalando e guardando os ficheiros no cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Limpando caches antigos (Gestão de Versão DevOps)
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+// Estratégia de busca: Tenta o cache, se não tiver, vai à rede
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
